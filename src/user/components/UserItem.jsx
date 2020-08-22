1bc4e7470713df0410dc37ebbdc1db6bbd0eeb6b
Nishu <nimish.prabhakar@eagleview.com>
2020-08-22 11:35:46 +0530

    Added the component folder

---
+++ src/user/components/UserItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Avatar from '../../shared/components/Avatar/Avatar';
+import Card from '../../shared/components/Card/Card';
+import './UserItem.css';
+
+const UserItem = ({ id, image, name, placeCount }) => {
+  return (
+    <li className="user-item">
+      <Card className="user-item__content">
+        <Link to={`/${id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <h3>
+              {placeCount} {placeCount === 1 ? 'Place' : 'Places'}
+            </h3>
+          </div>
+        </Link>
+      </Card>
+    </li>
+  );
+};
+
+export default UserItem;
