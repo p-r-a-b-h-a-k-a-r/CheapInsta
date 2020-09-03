a5c195c14b675aef08333b0f66efedd9ea69f75a
Nishu <nimish.prabhakar@eagleview.com>
2020-09-03 16:52:48 +0530

    added a context

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch,
 } from 'react-router-dom';
+import { AuthContext } from './shared/context/auth-context';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
+import Auth from './user/pages/Auth';
 import UpdatePlace from './places/pages/UpdatePlace';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route exact path="/" component={Users} />
-          <Route exact path="/:userID/places" component={UserPlaces} />
-          <Route exact path="/places/new" component={NewPlace} />
-          <Route path="/places/:placeId" component={UpdatePlace} />
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route exact path="/" component={Users} />
+            <Route exact path="/:userID/places" component={UserPlaces} />
+            <Route exact path="/places/new" component={NewPlace} />
+            <Route path="/places/:placeId" component={UpdatePlace} />
+            <Route exact path="/auth" component={Auth} />
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
